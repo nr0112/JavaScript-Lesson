59f7cf0f61ce6ebd89540fc51b36a6e83b8f3289
yunari26 <uawasiran3@gmail.com>
2023-03-26 13:12:09 +0900

    JavaScript第21回まで

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -1,20 +1,11 @@
 'use strict';
 
 {
-
-  document.querySelector('button').addEventListener('click', () => {
-    const colors = document.querySelectorAll('input');
-    const selectedColors = [];
-
-    colors.forEach(color => {
-      if (color.checked === true) {
-        selectedColors.push(color.value);
-      }
-    });
-
-    const li = document.createElement('li');
-    // li.textContent = selectedColors.join(',');
-    li.textContent = selectedColors;
-    document.querySelector('ul').appendChild(li);
-  });
+ document.querySelector('ul').addEventListener('click', e => {
+  if (e.target.nodeName === 'LI') {
+    e.target.classList.toggle('done');
+  }
+ });
 }
+
+
